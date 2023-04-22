89d8c7c24a2f71111f1cae39db00dd048a01e274
aleizn <119465229+aleizn@users.noreply.github.com>
2023-04-22 15:40:28 -0500

    Aleizn

--- comandos/gc-tagall.js
+++ comandos/gc-tagall.js
@@ -1,20 +1,20 @@
-let handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail("admin", m, conn);
-    throw false;
+  global.dfail('admin', m, conn)
+  throw false
   }
-  let pesan = args.join` `;
-  let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n🔥⃟⃪ᩙᤢᮬ⃪⃫᮪✑ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬:*\n`;
+  let pesan = args.join` `
+  let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+  let teks = `Creador info DM Wa.me/51992621601 no doy autógrafosxd
+  *⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
   for (let mem of participants) {
-    teks += `│*𓏲⊹ ⌑  @${mem.id.split("@")[0]}\n`;
+  teks += `┣👻 @${mem.id.split('@')[0]}\n`}
+  teks += `└𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 𝐀𝐥𝐞`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
-  teks += `*└*†𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤†\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)});
-};
-handler.help = ["tagall <mesaje>", "invocar <mesaje>"];
-handler.tags = ["group"];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
